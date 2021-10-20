e79ee2c6d88a4be5d384838d5323ecf1a7e1bd00
MariskaWanderer <mariska1.steyn@gmail.com>
2021-10-20 21:41:10 +0200

    Updated the icon current code

--- src/app.js
+++ src/app.js
@@ -26,15 +26,24 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute9(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function search(city) {
   let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let city = "London";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
+  ${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayTemperature);
 }
 
