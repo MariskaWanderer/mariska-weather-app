0ba2df0ca2edcf983c29d70fe639a6a1b752e8ed
MariskaWanderer <mariska1.steyn@gmail.com>
2021-10-20 22:38:14 +0200

    updated the axios link, rearranged the form

--- src/app.js
+++ src/app.js
@@ -24,12 +24,14 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute9(
@@ -40,7 +42,6 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
-  let city = "London";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
   ${apiKey}&units=metric`;
 
@@ -53,5 +54,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-let form = document.querySelector("search-form");
+search("New York");
+
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
