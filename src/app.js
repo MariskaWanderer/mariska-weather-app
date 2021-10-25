2948ccb0ad85c6235a703db6fb93954a38a2ab98
MariskaWanderer <mariska1.steyn@gmail.com>
2021-10-25 22:06:41 +0200

    Added the API for weather forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = "";
@@ -51,6 +51,12 @@ forecastHTML = forecastHTML + `
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -69,6 +75,8 @@ function displayTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -109,4 +117,3 @@ form.addEventListener("submit", handleSubmit);
 //celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
