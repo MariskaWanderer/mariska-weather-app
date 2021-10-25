71edcee63b09049c9c3b9692b0d503f975d8caf5
MariskaWanderer <mariska1.steyn@gmail.com>
2021-10-25 22:43:19 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -134,5 +134,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("New York");
